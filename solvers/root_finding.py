91e65d293db41af97ce8a51f8109244dd3d75bd7
hosboglot <ms581654@yandex.ru>
2025-01-09 22:27:21 +0300

    optimize root finding methods to store previous f evaluations

--- solvers/root_finding.py
+++ solvers/root_finding.py
@@ -15,18 +15,20 @@ class SecantSolver(RootSolverBase):
                  func: Callable[[float], float],
                  t1: float, t2: float):
         self._f = func
+
+        self._fs = [self._f(t1), self._f(t2)]
         self._ts = [t1, t2]
 
     def solution(self):
         return np.asarray(self._ts)
 
     def step(self):
-        diff = (self._f(self._ts[-1]) - self._f(self._ts[-2])) / \
-                    (self._ts[-1] - self._ts[-2])
-        self._ts.append(
-            self._ts[-2] -
-            self._f(self._ts[-2]) / diff
-            )
+        diff = (self._fs[-1] - self._fs[-2]) / \
+            (self._ts[-1] - self._ts[-2])
+        t = self._ts[-2] - self._fs[-2] / diff
+
+        self._ts.append(t)
+        self._fs.append(self._f(t))
         return self._ts[-2:]
 
 
@@ -37,18 +39,19 @@ class NewtonSolver(RootSolverBase):
                  t_last: float):
         self._f = func
         self._f_d = func_d
+
+        self._fs = [self._f(t_last)]
+        self._fs_d = [self._f_d(t_last)]
         self._ts = [t_last]
 
     def solution(self):
         return np.asarray(self._ts)
 
     def step(self):
-        f = self._f(self._ts[-1])
-        f_d = self._f_d(self._ts[-1])
-        self._ts.append(
-            self._ts[-1] -
-            f / f_d
-            )
+        t = self._ts[-1] - self._fs[-1] / self._fs_d[-1]
+        self._ts.append(t)
+        self._fs.append(self._f(t))
+        self._fs_d.append(self._f_d(t))
         return self._ts[-2:]
 
 
@@ -61,18 +64,21 @@ class HalleySolver(RootSolverBase):
         self._f = func
         self._f_d = func_d
         self._f_d2 = func_d2
+
+        self._fs = [self._f(t_last)]
+        self._fs_d = [self._f_d(t_last)]
+        self._fs_d2 = [self._f_d2(t_last)]
         self._ts = [t_last]
 
     def solution(self):
         return np.asarray(self._ts)
 
     def step(self):
-        f = self._f(self._ts[-1])
-        f_d = self._f_d(self._ts[-1])
-        f_d2 = self._f_d2(self._ts[-1])
-        self._ts.append(
-            self._ts[-1] -
-            (2 * f * f_d) /
-            (2 * f_d**2 - f * f_d2)
-            )
+        t = self._ts[-1] - (2 * self._fs[-1] * self._fs_d[-1]) / \
+            (2 * self._fs_d[-1]**2 - self._fs[-1] * self._fs_d2[-1])
+
+        self._ts.append(t)
+        self._fs.append(self._f(t))
+        self._fs_d.append(self._f_d(t))
+        self._fs_d2.append(self._f_d2(t))
         return self._ts[-2:]
